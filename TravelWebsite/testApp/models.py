bcc7f63684a1711fcdd951efb81494cd464478c0
zlczlc <zlchaha@gmail.com>
2018-02-04 22:26:27 -0800

    Revmove code that is not needed

--- TravelWebsite/testApp/models.py
+++ TravelWebsite/testApp/models.py
@@ -9,11 +9,6 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 # Create your models here.
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
-
 class WebUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth = models.DateField('birth', null=True)
