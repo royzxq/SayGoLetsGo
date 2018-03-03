72e725c5d131011c4793f7ea76aefab6567db04b
royzxq <royzxq@gmail.com>
2018-03-02 16:31:28 -0800

    add user search

---
+++ testVue/src/components/UserList.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+    <input type="text" v-model="search" placeholder="search username" />
+    <div v-for="user in filteredList">
+      Username: {{user.username}}
+    </div>
+	
+	</div>
+</template>
+
+<script>
+
+import {mapGetters} from 'vuex'
+export default {
+
+  name: 'UserList',
+  data () {
+    return {
+      // filter_list : [],
+      search: "",
+    }
+  },
+  methods:{      
+      goBack: function(){
+          this.$router.go(-1);
+      }
+  },
+  computed: {
+    ...mapGetters({
+      users: 'user/getUsers'
+    }),
+    filteredList(){
+      return this.users.filter(user => {
+        return user.username.toLowerCase().includes(this.search.toLowerCase())
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
